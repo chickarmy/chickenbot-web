8ab92eedcdb246b610b58b91e82462e161d6586d
Brice Vandeputte <brice.vandeputte@orange.com>
2024-10-07 21:20:52 +0200

    Fix #4 follow Docusaurus tutorial to init chicken-bot
    - follow docusaurus-tutorial and replace some resources
    - add one blog example
    - i18n en,fr
    - search

---
+++ src/pages/download.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Download() {
+    return (
+        <Layout>
+            <h1>Bot download page</h1>
+            <p>Please be patient ^^.</p>
+        </Layout>
+    );
+}
