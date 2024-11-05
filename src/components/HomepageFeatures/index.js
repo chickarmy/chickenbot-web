ecbe6a07c17e14a2b9190a48b4db5a8948baec33
Brice Vandeputte <brice.vandeputte@orange.com>
2024-11-05 20:09:19 +0100

    Fix #36 homepage translate feature, upd dep

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 const FeatureList = [
     {
@@ -43,19 +44,21 @@ const FeatureList = [
         description: (
             <>
                 With Chicken-bot, expand the horizon of your Minecraft adventures
-                by exploring new automated and fascinating worlds! "
+                by exploring new automated and fascinating worlds!
             </>
         ),
         description_fr: (
             <>
                 Avec Chicken-bot, élargissez l'horizon de vos aventures
-                Minecraft en explorant de nouveaux mondes automatisés et fascinants !"
+                Minecraft en explorant de nouveaux mondes automatisés et fascinants !
             </>
         ),
     }
 ];
 
-function Feature({Svg, Img, title, description}) {
+function Feature({locale, Svg, Img, title, title_fr, description, description_fr}) {
+    const titleToShow = locale === 'fr' ? title_fr : title;
+    const descriptionToShow = locale === 'fr' ? description_fr : description;
     return (
         <div className={clsx('col col--4')}>
             <div className="text--center">
@@ -63,20 +66,22 @@ function Feature({Svg, Img, title, description}) {
                 {Img && (<img src={Img.src} alt={Img.alt}/>)}
             </div>
             <div className="text--center padding-horiz--md">
-                {title && (<Heading as="h3">{title}</Heading>)}
-                <p>{description}</p>
+                {title && (<Heading as="h3">{titleToShow}</Heading>)}
+                <p>{descriptionToShow}</p>
             </div>
         </div>
     );
 }
 
 export default function HomepageFeatures() {
+    const {i18n } = useDocusaurusContext();
+    const currentLocale = i18n.currentLocale;
     return (
         <section className={styles.features}>
             <div className="container">
                 <div className="row">
                     {FeatureList.map((props, idx) => (
-                        <Feature key={idx} {...props} />
+                        <Feature key={idx} locale={currentLocale} {...props} />
                     ))}
                 </div>
             </div>
