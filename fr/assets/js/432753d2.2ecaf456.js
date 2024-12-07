ddfc313885ca655e00648e515e198c6d6c77e089
boly38 <boly38@users.noreply.github.com>
2024-12-07 14:11:58 +0000

    deploy: 2f854bf8b27f055effb081571efb5d2da5d5fec3

--- fr/assets/js/432753d2.2ecaf456.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkchickenbot_web=self.webpackChunkchickenbot_web||[]).push([[4847],{2149:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/chickenbot-web/fr/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
